41731b74166c4423164548ccbb0f355d02c9ff6c
EzequielGhR <ezequielghra97@gmail.com>
2025-11-24 11:53:41 -0300

    Updated defaults and error handling. Added possibility of providing parameters to the command

--- updater.sh
+++ updater.sh
@@ -10,9 +10,42 @@ PLATFORM="linux-x64"
 RELEASE_TRACK="stable"
 OLD_APP_IMAGE_PATH="/opt/cursor/cursor*.AppImage"
 TEMP_DIR="${HOME}/cursor_temp"
-NEW_APP_IMAGE_PATH="${TEMP_DIR}/cursor.AppImage"
+NEW_APP_IMAGE_PATH="/opt/cursor/cursor.AppImage"
 
 function update-cursor() {
+	while [ $# -gt 0 ]
+		do case $1 in
+			--platform)
+				PLATFORM=$2
+				shift 2
+			;;
+			--release-track)
+				RELEASE_TRACK=$2
+				shift 2
+			;;
+			--old-app-image-path)
+				OLD_APP_IMAGE_PATH=$2
+				shift 2
+			;;
+			--new-app-image-path)
+				NEW_APP_IMAGE_PATH=$2
+				shift 2
+			;;
+			--temp-dir)
+				TEMP_DIR=$2
+				shift 2
+			;;
+			--help)
+				echo "Usage: update-cursor [--platform <platform>] [--release-track <release-track>] [--old-app-image-path <old-app-image-path>] [--new-app-image-path <new-app-image-path>] [--temp-dir <temp-dir>]"
+				return 0
+			;;
+			*)
+				echo "Unknown option: $1"
+				return 1
+			;;
+		esac
+	done
+
 	echo "Checking cursor is not running"
 	ps -A | grep -q cursor && {
 		echo "Cannot update cursor because it is currently running"
@@ -43,8 +76,8 @@ function update-cursor() {
 		return 1
 	fi
 
-	sudo rm -rf "${OLD_APP_IMAGE_PATH}" && sudo mv "${TEMP_DIR}/cursor.AppImage" "${NEW_APP_IMAGE_PATH}"
-	sudo chmod +x "${NEW_APP_IMAGE_PATH}"
+	sudo rm $OLD_APP_IMAGE_PATH || echo "Failed to delete old file: $OLD_APP_IMAGE_PATH"
+	sudo mv "${TEMP_DIR}/cursor.AppImage" "${NEW_APP_IMAGE_PATH}" && sudo chmod +x "${NEW_APP_IMAGE_PATH}"
 
 	let success=$?
 	if [ $success -eq 0 ]
